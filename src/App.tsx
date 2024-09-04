d1c32f365add2724fdfa335d63e253bc76699fe2
Basyaact <2095657527@qq.com>
2024-09-04 13:26:19 +0800

    添加了一些引用，稍微更改了webpack

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,10 @@ import { QueryClientProvider, QueryClient } from 'react-query';
 import LayoutApp from './components/LayoutApp';
 import ReverseShell from './components/linux/ReverseShell';
 import './assets/css/style.css';
+import 'antd/dist/antd.css';
+import 'antd/dist/antd.variable.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 const queryClient = new QueryClient();
 
